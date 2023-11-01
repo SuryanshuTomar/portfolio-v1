be851a480e23cbbbec34d17ca37ffd5f7a71ae27
eshkon-suryanshu <eshkon.suryanshu@gmail.com>
2023-11-01 17:55:21 +0530

    add : AdminExperienceView comp

--- src/components/admin-view/experience/index.tsx
+++ src/components/admin-view/experience/index.tsx
@@ -1,5 +1,26 @@
 "use client";
 
-export default function AdminExperienceView() {
-	return <div>AdminExperienceView</div>;
+import type { FormControlsType } from "@/Types";
+
+import FormControls from "../form-controls";
+
+export default function AdminExperienceView({
+	controls,
+	formData,
+	setFormData,
+}: FormControlsType) {
+	return (
+		<div className="w-full">
+			<div className="bg-[#ffffff] shadow-md rounded px-8 pt-6 pb-8 mb-4">
+				<FormControls
+					controls={controls}
+					formData={formData}
+					setFormData={setFormData}
+				/>
+				<button className="mt-10 border border-green-600 p-4 font-bold text-[16px]">
+					Add Info
+				</button>
+			</div>
+		</div>
+	);
 }
