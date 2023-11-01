9da8a948ed7eb1706fc50bfbcb57e4d1146915cc
eshkon-suryanshu <eshkon.suryanshu@gmail.com>
2023-11-01 13:07:23 +0530

    add: initial commit

---
+++ src/config/default.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+	dbUri: "mongodb://127.0.0.1:27017/portfolio-v1",
+};
---
+++ src/database/index.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import config from "config";
+
+export default async function connectDb(): Promise<void> {
+	const dbUri = config.get<string>("dbUri");
+
+	try {
+		await mongoose.connect(dbUri);
+	} catch (error) {
+		console.log("Failed to connect with DB!", error);
+	}
+}
