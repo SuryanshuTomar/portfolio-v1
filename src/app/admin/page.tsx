9e1d9866d8fe794a54e51bf76f8cacfab976abd7
eshkon-suryanshu <eshkon.suryanshu@gmail.com>
2023-11-01 17:09:16 +0530

    add: render home controls in HomeViewComponent

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,44 +1,57 @@
 "use client";
 
 import { useState } from "react";
+
+import type { FormDataType, MenuIds } from "@/Types";
+import { controls } from "@/utils";
+
 import { menuItems } from "./utils";
+
+const initialHomeFormData: FormDataType = {
+	heading: "",
+	summary: "",
+};
 
 export default function AdminView() {
 	const [currentSelectedTab, setCurrentSelectedTab] =
 		useState<MenuIds>("home");
 
+	const [homeViewFormData, setHomeViewFormData] =
+		useState<FormDataType>(initialHomeFormData);
+
 	// Navbar Component
-	const Navbar = () =>
-		menuItems.map((menuItem) => (
-			<button
-				key={menuItem.id}
-				type="button"
-				className="p-4 font-bold text-xl text-black"
-				onClick={() => setCurrentSelectedTab(menuItem.id)}
-			>
-				{menuItem.label}
-			</button>
-		));
+	const Navbar = menuItems.map((menuItem) => (
+		<button
+			key={menuItem.id}
+			type="button"
+			className="p-4 font-bold text-xl text-black"
+			onClick={() => setCurrentSelectedTab(menuItem.id)}
+		>
+			{menuItem.label}
+		</button>
+	));
 
 	// View Component
-	const ViewComponent = () =>
-		menuItems.map((menuItem) =>
-			menuItem.id === currentSelectedTab ? (
-				<menuItem.component key={menuItem.id} />
-			) : undefined
-		);
+	const ViewComponent = menuItems.map((menuItem) =>
+		menuItem.id === currentSelectedTab ? (
+			<menuItem.component
+				key={menuItem.id}
+				controls={controls[menuItem.id]}
+				formData={homeViewFormData}
+				setFormData={setHomeViewFormData}
+			/>
+		) : undefined
+	);
 
 	return (
 		<div className="border-b border-gray-200">
 			<nav className="-mb-0.5 flex justify-center space-x-6" role="tablist">
 				{/* Rendering Navbar */}
-				<Navbar />
+				{Navbar}
 			</nav>
 
-				{/* Rendering View Components */}
-				<div className="mt-10 p-10">
-					<ViewComponent />
-				</div>
+			{/* Rendering View Components */}
+			<div className="mt-10 p-10">{ViewComponent}</div>
 		</div>
 	);
 }
--- src/components/admin-view/home/index.tsx
+++ src/components/admin-view/home/index.tsx
@@ -1,6 +1,26 @@
-"use client"
+"use client";
 
-export default function AdminHomeView(){
+import type { FormControlsType } from "@/Types";
 
-  return <div>AdminHomeView</div>
+import FormControls from "../form-controls";
+
+export default function AdminHomeView({
+	controls,
+	formData,
+	setFormData,
+}: FormControlsType) {
+	return (
+		<div className="w-full">
+			<div className="bg-[#ffffff] shadow-md rounded px-8 pt-6 pb-8 mb-4">
+				<FormControls
+					controls={controls}
+					formData={formData}
+					setFormData={setFormData}
+				/>
+				<button className="mt-10 border border-green-600 p-4 font-bold text-[16px]">
+					Add Info
+				</button>
+			</div>
+		</div>
+	);
 }
