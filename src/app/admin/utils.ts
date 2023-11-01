d1b2e50c8db2129a38358d0b72b2954be7f12283
eshkon-suryanshu <eshkon.suryanshu@gmail.com>
2023-11-01 14:44:21 +0530

    move: MenuItems to utils

---
+++ src/app/admin/utils.ts
@@ -0,0 +1,39 @@
+import AdminAboutView from "@/components/admin-view/about";
+import AdminContactView from "@/components/admin-view/contact";
+import AdminEducationView from "@/components/admin-view/education";
+import AdminExperienceView from "@/components/admin-view/experience";
+import AdminHomeView from "@/components/admin-view/home";
+import AdminProjectsView from "@/components/admin-view/projects";
+
+export const menuItems: MenuItem[] = [
+	{
+		id: "home",
+		label: "Home",
+		component: AdminHomeView,
+	},
+	{
+		id: "about",
+		label: "About",
+		component: AdminAboutView,
+	},
+	{
+		id: "experience",
+		label: "Experience",
+		component: AdminExperienceView,
+	},
+	{
+		id: "education",
+		label: "Education",
+		component: AdminEducationView,
+	},
+	{
+		id: "projects",
+		label: "Projects",
+		component: AdminProjectsView,
+	},
+	{
+		id: "contact",
+		label: "Contact",
+		component: AdminContactView,
+	},
+];
