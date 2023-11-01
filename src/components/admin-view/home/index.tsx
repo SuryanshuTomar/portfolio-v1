af2903f64baca12875fec86c77b55dab23b3e21e
eshkon-suryanshu <eshkon.suryanshu@gmail.com>
2023-11-01 13:36:35 +0530

    add: navbar components for admin-view

---
+++ src/components/admin-view/home/index.tsx
@@ -0,0 +1,6 @@
+"use client"
+
+export default function AdminHomeView(){
+
+  return <div>AdminHomeView</div>
+}
---
+++ src/components/admin-view/projects/index.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function AdminProjectsView() {
+	return <div>AdminProjectsView</div>;
+}
