9abda2644452fac7c306a8d6231d1cb1b0253add
eshkon-suryanshu <eshkon.suryanshu@gmail.com>
2023-11-01 17:54:34 +0530

    add: initialData for FormsData

--- src/app/admin/utils.ts
+++ src/app/admin/utils.ts
@@ -4,7 +4,7 @@ import AdminEducationView from "@/components/admin-view/education";
 import AdminExperienceView from "@/components/admin-view/experience";
 import AdminHomeView from "@/components/admin-view/home";
 import AdminProjectsView from "@/components/admin-view/projects";
-import type { MenuItem } from "@/Types";
+import type { FormDataType, MenuItem } from "@/Types";
 
 export const menuItems: MenuItem[] = [
 	{
@@ -38,3 +38,42 @@ export const menuItems: MenuItem[] = [
 		component: AdminContactView,
 	},
 ];
+
+export const initialHomeFormData: FormDataType = {
+	heading: "",
+	summary: "",
+};
+
+export const initialAboutFormData: FormDataType = {
+	aboutme: "",
+	noofprojects: "",
+	yearofexperience: "",
+	noofclients: "",
+	skills: "",
+};
+
+export const initialExperienceFormData: FormDataType = {
+	position: "",
+	company: "",
+	duration: "",
+	location: "",
+	jobprofile: "",
+};
+
+export const initialEducationFormData: FormDataType = {
+	degree: "",
+	year: "",
+	college: "",
+};
+
+export const initialProjectsFormData: FormDataType = {
+	name: "",
+	website: "",
+	technologies: "",
+	github: "",
+};
+
+export const initialLoginFormData: FormDataType = {
+	username: "",
+	password: "",
+};
