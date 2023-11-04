823e35cfeec93f459bf6519d3fb48d900a346174
eshkon-suryanshu <eshkon.suryanshu@gmail.com>
2023-11-04 18:31:04 +0530

    add :  addData and getData for the tabs

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+import { addData, getData } from "@/services";
 import type { FormDataType, MenuIds } from "@/Types";
 import { controls } from "@/utils";
 
@@ -17,6 +18,8 @@ import { menuItems } from "./utils";
 export default function AdminView() {
 	const [currentSelectedTab, setCurrentSelectedTab] =
 		useState<MenuIds>("home");
+
+	const [allTabsData, setAllTabsData] = useState({});
 
 	const [homeViewFormData, setHomeViewFormData] =
 		useState<FormDataType>(initialHomeFormData);
@@ -35,55 +38,124 @@ export default function AdminView() {
 		}
 	);
 
+	const getFormData = (id: MenuIds) => {
+		let result = homeViewFormData;
+		switch (id) {
+			case "home":
+				result = homeViewFormData;
+				break;
+			case "about":
+				result = aboutViewFormData;
+				break;
+			case "experience":
+				result = experienceViewFormData;
+				break;
+			case "education":
+				result = educationViewFormData;
+				break;
+			case "projects":
+				result = projectsViewFormData;
+				break;
+			case "contact":
+				result = contactViewFormData;
+				break;
+		}
+		return result;
+	};
+
+	const getSetFormData = (id: MenuIds) => {
+		let result = setHomeViewFormData;
+		switch (id) {
+			case "home":
+				result = setHomeViewFormData;
+				break;
+			case "about":
+				result = setAboutViewFormData;
+				break;
+			case "experience":
+				result = setExperienceViewFormData;
+				break;
+			case "education":
+				result = setEducationViewFormData;
+				break;
+			case "projects":
+				result = setProjectsViewFormData;
+				break;
+			case "contact":
+				result = setContactViewFormData;
+				break;
+		}
+		return result;
+	};
+
+	// Reset Forms
+	function resetFormsData() {
+		setHomeViewFormData(initialHomeFormData);
+		setAboutViewFormData(initialAboutFormData);
+		setExperienceViewFormData(initialExperienceFormData);
+		setEducationViewFormData(initialEducationFormData);
+		setProjectsViewFormData(initialProjectsFormData);
+		setContactViewFormData({
+			email: "",
+			contactInfo: "",
+		});
+	}
+
+	// This function will get the selected tab data and save it to the allTabsData state.
+	async function getTabsData(currentTab: MenuIds) {
+		const response = await getData(currentTab);
+
+		if (
+			response !== null &&
+			response !== undefined &&
+			response?.data !== null &&
+			response?.data !== undefined
+		) {
+			if (Array.isArray(response?.data) && response?.data.length > 0) {
+				if (currentTab === "home") {
+					setHomeViewFormData(response.data[0]);
+				} else if (currentTab === "about") {
+					setAboutViewFormData(response.data[0]);
+				}
+			}
+		}
+
+		if (response !== null || response !== undefined) {
+			setAllTabsData({
+				...allTabsData,
+				[currentTab]: response?.data,
+			});
+		}
+	}
+	// This function will be called to save the form data
+	async function handleSaveData(
+		tabName?: MenuIds
+	): Promise<FormDataType | null> {
+		const currentTab = tabName ?? currentSelectedTab;
+		const response = await addData(currentTab, getFormData(currentTab));
+
+		if (response !== null && response !== undefined) {
+			resetFormsData();
+			getTabsData(currentSelectedTab);
+		}
+
+		return response;
+	}
+
 	// Navbar Component
 	const Navbar = menuItems.map((menuItem) => (
 		<button
 			key={menuItem.id}
 			type="button"
 			className="p-4 font-bold text-xl text-black"
-			onClick={() => setCurrentSelectedTab(menuItem.id)}
+			onClick={() => {
+				setCurrentSelectedTab(menuItem.id);
+				resetFormsData();
+			}}
 		>
 			{menuItem.label}
 		</button>
 	));
-
-	const getFormData = (id: string) => {
-		let result = homeViewFormData;
-		switch (id) {
-			case "home":
-				result = homeViewFormData;
-			case "about":
-				result = aboutViewFormData;
-			case "experience":
-				result = experienceViewFormData;
-			case "education":
-				result = educationViewFormData;
-			case "projects":
-				result = projectsViewFormData;
-			case "contact":
-				result = contactViewFormData;
-		}
-		return result;
-	};
-
-	const getSetFormData = (id: string) => {
-		let result = setHomeViewFormData;
-		switch (id) {
-			case "home":
-				result = setHomeViewFormData;
-			case "about":
-				result = setAboutViewFormData;
-			case "experience":
-				result = setExperienceViewFormData;
-			case "education":
-				result = setEducationViewFormData;
-			case "projects":
-				result = setProjectsViewFormData;
-			case "contact":
-				result = setContactViewFormData;
-		}
-		return result;
-	};
 
 	// View Component
 	const ViewComponent = menuItems.map((menuItem) =>
@@ -93,9 +165,15 @@ export default function AdminView() {
 				controls={controls[menuItem.id]}
 				formData={getFormData(menuItem.id)}
 				setFormData={getSetFormData(menuItem.id)}
+				handleSaveData={handleSaveData}
 			/>
 		) : undefined
 	);
+
+	useEffect(() => {
+		getTabsData(currentSelectedTab);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentSelectedTab]);
 
 	return (
 		<div className="border-b border-gray-200">
