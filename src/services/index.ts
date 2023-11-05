2745e71d1c3abb06b708692fb64b0ba67445fe83
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-11-05 17:55:41 +0530

    add: updateData service

--- src/services/index.ts
+++ src/services/index.ts
@@ -31,3 +31,25 @@ export async function getData(currentTab: MenuIds): Promise<ApiResponseType | nu
 	}
 	return result;
 }
+
+export async function updateData(
+	currentTab: MenuIds,
+	formData: FormDataType
+): Promise<ApiResponseType | null> {
+	let result: ApiResponseType | null = null;
+
+	try {
+		const response = await fetch(`/api/${currentTab}/update`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(formData),
+		});
+
+		result = await response.json();
+	} catch (error) {
+		console.log(error);
+	}
+	return result;
+}
