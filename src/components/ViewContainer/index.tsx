2ad0d08c8535596c1ebe5ac5a68b7dc3fa50080f
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-12-20 21:56:03 +0530

    add: ViewContainer Component

---
+++ src/components/ViewContainer/index.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import type { HTMLAttributes, FC, ReactNode } from "react";
+
+interface ViewContainerProps extends HTMLAttributes<HTMLDivElement> {
+	children: ReactNode;
+}
+
+const ViewContainer: FC<ViewContainerProps> = ({ children, className }) => {
+	const containerClass = [
+		"bg-primaryBg shadow-lg shadow-onPrimaryBg rounded px-8 pt-6 pb-8 mb-4",
+		className,
+	]
+		.filter(Boolean)
+		.join(" ");
+
+	return <div className={containerClass}>{children}</div>;
+};
+export default ViewContainer;
