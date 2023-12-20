e9c768d4a0e5907979e28d5e64d65219e5baf41e
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-12-20 21:10:27 +0530

    add: using colors using the new themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			colors: {
+				onNeutralBg: "var(--onNeutralBg)",
+				neutralBg: "var(--neutralBg)",
+				onPrimaryBg: "var(--onPrimaryBg)",
+				primaryBg: "var(--primaryBg)",
+				primary: "var(--primary)",
+				secondary: "var(--secondary)",
+				tertiary: "var(--tertiary)",
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
