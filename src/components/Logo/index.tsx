fda73eb7536a7d9b7e5a1dc6576f49c3d82fbd7f
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-12-21 22:54:14 +0530

    add: Logo Component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+	return (
+		<div className="w-content h-20 flex justify-center items-center p-3 rounded-[8px] border-primaryBg text-lg text-onPrimaryBg">
+			<span className="text-primary text-[30px] font-bold">P</span>
+			ortfolio
+		</div>
+	);
+};
+export default Logo;
