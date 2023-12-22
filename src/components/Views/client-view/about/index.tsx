33cc417b1b42525881ec8578d1a7340a82a11e6e
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-12-22 22:43:57 +0530

    add: component design

--- src/components/Views/client-view/about/index.tsx
+++ src/components/Views/client-view/about/index.tsx
@@ -1,11 +1,114 @@
-import type { FC } from "react";
+"use client";
 
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { useMemo, type FC } from "react";
+
+import AboutMeImage from "@/../public/code.svg";
 import type { ClientViewProps } from "@/Types";
+import { getVariants } from "@/utils";
+
+import AnimationWrapper from "../wrapper";
 
 interface AboutViewProps extends ClientViewProps {}
 
-const AboutView: FC<AboutViewProps> = (props) => {
-	return <div>AboutView</div>;
+const AboutView: FC<AboutViewProps> = ({ data }) => {
+	const memoisedVariants = useMemo(() => getVariants(), []);
+
+	const aboutDataInfo = [
+		{
+			label: "Clients",
+			value: data?.noofclients || "0",
+		},
+		{
+			label: "Projects",
+			value: data?.noofprojects || "0",
+		},
+		{
+			label: "Experience",
+			value: data?.yearofexperience || "0",
+		},
+	];
+
+	const heading = "Why hire me for your next project?"
+		.split(" ")
+		.map((item, index) => (
+			<span
+				key={index}
+				className={`${index === 6 ? "text-tertiary" : "text-primary"}`}
+			>
+				{item}{" "}
+			</span>
+		));
+
+	return (
+		<div className="max-w-screen-xl mt-24 mb-6 sm:mt-14 px-6 sm:px-8 lg:px-16 mx-auto">
+			<div className="w-full flex">
+				<AnimationWrapper className="w-full grid-flow-row grid grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-tertiary bg-white-500 z-10">
+					{aboutDataInfo.map((infoItem, index) => (
+						<motion.div
+							key={infoItem.label}
+							custom={{ duration: 2 + index }}
+							variants={memoisedVariants}
+							className={`flex items-center justify-start 
+						${
+							index === 0
+								? "sm:justify-start"
+								: index === 1
+								? "sm:justify-center"
+								: "sm:justify-end"
+						}
+						py-4 sm:py-6 w-8/12 px-4 sm:w-auto mx-auto sm:mx-0
+					`}
+						>
+							<div className="flex m-0 w-40 sm:w-auto">
+								<div className="flex flex-col">
+									<p className="text-6xl text-primary font-bold">
+										{infoItem.value}+
+									</p>
+									<p className="text-3xl text-onPrimaryBg font-bold">
+										{infoItem.label}
+									</p>
+								</div>
+							</div>
+						</motion.div>
+					))}
+				</AnimationWrapper>
+			</div>
+
+			<AnimationWrapper className="pt-5">
+				<div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1">
+					<h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-medium">
+						{heading}
+					</h1>
+					<p className="text-secondary mt-4 mb-8 font-bold">
+						{data?.aboutme}
+					</p>
+				</div>
+			</AnimationWrapper>
+
+			<div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8">
+				<AnimationWrapper className="flex w-full">
+					<motion.div
+						variants={memoisedVariants}
+						className="h-full w-full p-4"
+					>
+						<Image
+							src={AboutMeImage}
+							alt="About Me"
+							layout="responsive"
+							quality={100}
+							height={414}
+							width={500}
+						/>
+					</motion.div>
+				</AnimationWrapper>
+				<AnimationWrapper className="flex items-center w-full p-4">
+						
+				</AnimationWrapper>
+			</div>
+		</div>
+	);
 };
 
 export default AboutView;
--- src/components/Views/client-view/home/index.tsx
+++ src/components/Views/client-view/home/index.tsx
@@ -1,11 +1,109 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { useMemo, useRef } from "react";
 import type { FC } from "react";
 
 import type { ClientViewProps } from "@/Types";
+import { getVariants } from "@/utils";
+
+import { socialIcons } from "../../utils";
+import AnimationWrapper from "../wrapper";
+
+import ProfilePic from "@/../public/mypic-bg-removed.png";
 
 interface HomeViewProps extends ClientViewProps {}
 
-const HomeView: FC<HomeViewProps> = (props) => {
-	return <div>HomeView</div>;
+const HomeView: FC<HomeViewProps> = ({ data }) => {
+	const dpContainerRef = useRef<HTMLDivElement>(null);
+	const memoisedVariants = useMemo(() => getVariants(), []);
+
+	// const isDataPresent = data && Array.isArray(data);
+	const headingData = data?.heading;
+	const summaryData = data?.summary;
+
+	const heading =
+		typeof headingData === "string"
+			? headingData.split(" ").map((item, index) => (
+					<span
+						key={index}
+						className={`${
+							index === 2 || index === 3
+								? "text-primary"
+								: "text-onPrimaryBg"
+						}`}
+					>
+						{item}{" "}
+					</span>
+			  ))
+			: null;
+
+	return (
+		<div className="max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto" id="home">
+			<AnimationWrapper>
+				<motion.div
+					className="flex flex-col-reverse md:flex-row justify-around items-center gap-x-16 "
+					variants={memoisedVariants}
+				>
+					<div className="flex flex-col justify-center items-start pt-20">
+						<h1 className="mb-4 text-3xl lg:text-4xl xl:text-6xl font-medium leading-normal">
+							{heading}
+						</h1>
+						<p className="text-secondary mt-4 mb-8 font-bold">
+							{summaryData}
+						</p>
+						<motion.div className="flex gap-3 cursor-pointer">
+							{socialIcons.map((item) => (
+								<motion.div
+									key={item.id}
+									initial={{ scale: 0 }}
+									animate={{ rotate: 360, scale: 1 }}
+									transition={{
+										type: "spring",
+										damping: 20,
+										stiffness: 80,
+										duration: 4,
+									}}
+									whileHover={{ scale: 1.2, rotate: 360 }}
+									whileTap={{
+										scale: 0.4,
+										rotate: -360,
+										borderRadius: "100%",
+									}}
+								>
+									{item.icon}
+								</motion.div>
+							))}
+						</motion.div>
+					</div>
+					<motion.div
+						ref={dpContainerRef}
+						className="flex w-full justify-center"
+					>
+						<motion.div
+							drag
+							dragConstraints={dpContainerRef}
+							className="w-80 h-80 md:w-96 md:h-96 relative rounded-lg bg-primary"
+						>
+							<div className="w-80 h-80 md:w-96 md:h-96 top-5 -left-5 rounded-lg border-[6px] border-secondary absolute flex justify-center items-center pl-6 ">
+								<div className="w-56 h-full rounded-[30px] border-6 border-secondary overflow-hidden ">
+									<Image
+										src={ProfilePic}
+										alt="Profile Pic"
+										layout="responsive"
+										quality={100}
+										objectFit="cover"
+									/>
+									<div className="absolute top-0 left-0 w-full h-full bg-transparent z-10"></div>
+								</div>
+							</div>
+						</motion.div>
+					</motion.div>
+				</motion.div>
+			</AnimationWrapper>
+		</div>
+	);
 };
 
 export default HomeView;
